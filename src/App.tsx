fa9a3152ed92eaaa9537629e48dacb1401403c0b
Luka Khutsishvili <lukakhutsishvili777@gmail.com>
2024-12-13 09:43:56 +0400

    sended request succesfully

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ import axios from "axios";
 function App() {
   const [foo, setFoo] = useState<string>("");
 
+  const chat_id = 6087086146;
+
   type createContextType = {
     foo: string;
     setFoo: any;
@@ -19,35 +21,34 @@ function App() {
     setFoo: () => {},
   });
 
+  const username = "telegram_bot";
+  const password = "657152";
 
-  const username = 'telegram_bot';
-  const password = '657152';
+  const encodedCredentials = btoa(`${username}:${password}`);
 
-const encodedCredentials = btoa(`${username}:${password}`);
+  const params: any = { telegram_id: chat_id };
 
-// params = {"telegram_id": chat_id}
-
-const axiosInstance = axios.create({
- baseURL: 'https://bo.delivo.ge/delivo_test/hs/bot',
- headers: {
-   Authorization: `Basic ${encodedCredentials}`,
- },
-});
-
+  const axiosInstance = axios.create({
+    baseURL: "https://bo.delivo.ge/delivo_test/hs/bot",
+    headers: {
+      Authorization: `Basic ${encodedCredentials}`,
+    },
+  });
 
   const handleSignIn = async () => {
-   try {
-     const response = await axiosInstance.get("/auth", { });
-       console.log(response);
-   } catch (error:any) {
-     console.log(`Error: ${error.message}`);
-   }
- };
- 
-
+    try {
+      const response = await axiosInstance.get("/auth", { params });
+      console.log(response.data);
+    } catch (error: any) {
+      console.log(`Error: ${error.message}`);
+    }
+  };
 
   return (
     <>
+      <div className=" hover:cursor-pointer" onClick={handleSignIn}>
+        request
+      </div>
       <Context.Provider value={{ foo, setFoo }}>
         <Routes>
           <Route element={<Home />} path="/home" />
