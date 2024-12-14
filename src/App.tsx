f0298f791cd76a5cf92f1227f6e73f7417783298
Luka Khutsishvili <lukakhutsishvili777@gmail.com>
2024-12-14 12:54:16 +0400

    expand screen

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,10 @@ import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import Reciept from "./pages/Reciept";
 import Sending from "./pages/Sending";
-import { createContext, useState } from "react";
-import axios from "axios";
+import { createContext, useEffect, useState } from "react";
 
 function App() {
   const [foo, setFoo] = useState<string>("");
-
-  const chat_id = 6087086146;
 
   type createContextType = {
     foo: string;
@@ -21,34 +18,21 @@ function App() {
     setFoo: () => {},
   });
 
-  const username = "telegram_bot";
-  const password = "657152";
+  useEffect(() => {
+    // Ensure the Telegram WebApp is initialized
+    if (window.Telegram.WebApp) {
+      const webApp = window.Telegram.WebApp;
 
-  const encodedCredentials = btoa(`${username}:${password}`);
+      // Notify Telegram the app is ready
+      webApp.ready();
 
-  const params: any = { telegram_id: chat_id };
-
-  const axiosInstance = axios.create({
-    baseURL: "https://bo.delivo.ge/delivo_test/hs/bot",
-    headers: {
-      Authorization: `Basic ${encodedCredentials}`,
-    },
-  });
-
-  const handleSignIn = async () => {
-    try {
-      const response = await axiosInstance.get("/auth", { params });
-      console.log(response.data);
-    } catch (error: any) {
-      console.log(`Error: ${error.message}`);
+      // Request to expand the app to full screen
+      webApp.expand();
     }
-  };
+  }, []);
 
   return (
     <>
-      <div className=" hover:cursor-pointer" onClick={handleSignIn}>
-        request
-      </div>
       <Context.Provider value={{ foo, setFoo }}>
         <Routes>
           <Route element={<Home />} path="/home" />
