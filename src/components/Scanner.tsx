92bc5ed64739d44ed24a443b1cbf1499e665c8f4
lukakhutsishvili <lukakhutsishvili777@gmail.com>
2024-12-19 12:45:35 +0400

    fix scanner

--- src/components/Scanner.tsx
+++ src/components/Scanner.tsx
@@ -3,25 +3,36 @@ import { useState } from "react";
 
 const BarcodeScanner = () => {
   const [result, setResult] = useState<string | null>(null);
+  const [isScanning, setIsScanning] = useState(true);
 
   const {
     ref,
     torch: { on, off, isOn, isAvailable },
   } = useZxing({
-    onDecodeResult: (result) => setResult(result.getText()), // Capture scanned result
+    onDecodeResult: (decodedResult) => {
+      setResult(decodedResult.getText()); // Capture scanned result
+      setIsScanning(false); // Stop scanning
+    },
   });
 
   return (
     <>
-      <video ref={ref} />
-      {isAvailable ? (
-        <button onClick={() => (isOn ? off() : on())}>
-          {isOn ? "Turn off" : "Turn on"} torch
-        </button>
+      {isScanning ? (
+        <>
+          <video ref={ref} />
+          {isAvailable ? (
+            <button onClick={() => (isOn ? off() : on())}>
+              {isOn ? "Turn off" : "Turn on"} torch
+            </button>
+          ) : (
+            <strong>
+              Unfortunately, torch is not available on this device.
+            </strong>
+          )}
+        </>
       ) : (
-        <strong>Unfortunately, torch is not available on this device.</strong>
+        <p>Scanned Result: {result}</p>
       )}
-      {result && <p>Scanned Result: {result}</p>}
     </>
   );
 };
