eb2b8d16bb32ddc4a8d105d71d8f42e55eaeb75c
Luka Khutsishvili <lukakhutsishvili777@gmail.com>
2024-12-26 01:32:59 +0400

    add

--- src/components/Scanner.tsx
+++ src/components/Scanner.tsx
@@ -30,7 +30,7 @@ const BarcodeScanner = () => {
       const response = await axiosInstance.get(GET_DETAILS_BY_SCANNER, {
         params,
       });
-      setResponseData(response.data.response.value);
+      setResponseData(response.data.response.value.tracking_codes);
       setIsModalOpen(true); // Open modal after data is fetched
     } catch (error) {
       console.error("Error fetching barcode details:", error);
