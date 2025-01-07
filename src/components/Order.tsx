d24dc24ecfe74b9f5a7ff3fde551d1a9d75a4018
Luka Khutsishvili <lukakhutsishvili777@gmail.com>
2025-01-08 02:19:00 +0400

    add sorting method

--- src/components/Order.tsx
+++ src/components/Order.tsx
@@ -7,6 +7,73 @@ import { t } from "i18next";
 import { changeOrderStatus } from "../api/requestHandlers";
 import { axiosInstance } from "../api/apiClient";
 import { ORDER_LIST } from "../api/Constants";
+import { closestCenter, DndContext } from "@dnd-kit/core";
+import {
+  SortableContext,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import { arrayMove } from "@dnd-kit/sortable";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+
+export const SortableItem = ({
+  id,
+  task,
+  status,
+  navigate,
+  handleCheckboxChange,
+  selectedOrders,
+}: any) => {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
+  return (
+    <div
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+      className="first:border-t-2 border-b-2 py-2 px-3 border-gray-500 flex gap-4"
+    >
+      {status === "Waiting" && (
+        <div className="flex items-center gap-2 mt-2">
+          <input
+            type="checkbox"
+            checked={!!selectedOrders[task.tracking_code]}
+            onChange={(e) =>
+              handleCheckboxChange(task.tracking_code, e.target.checked)
+            }
+            className="h-5 w-5 text-yellow-600 rounded border-gray-300 focus:ring-yellow-500"
+          />
+        </div>
+      )}
+
+      <div
+        className="w-full flex flex-col gap-1 cursor-pointer"
+        onClick={() => navigate(`/order/${task.tracking_code}`)}
+      >
+        <div className="flex justify-between">
+          <h2 className="text-sm">{task.client_name}</h2>
+          <p className="text-sm">{task.sum} ₾</p>
+        </div>
+        <div className="flex items-center gap-1">
+          <FontAwesomeIcon icon={faBarcode} />
+          <p className="text-sm">{task.tracking_code}</p>
+        </div>
+        <div className="flex items-center gap-1">
+          <FontAwesomeIcon icon={faPhone} />
+          <p className="text-sm">{task.client_phone}</p>
+        </div>
+        <h2 className="text-sm">{task.client_address}</h2>
+      </div>
+    </div>
+  );
+};
 
 const Order = ({ status }: { status: string | null }) => {
   const { sendingTasks, userInfo, setSendingTasks } = useContext(Context);
@@ -71,46 +138,24 @@ const Order = ({ status }: { status: string | null }) => {
     }
   };
 
-  const handleAllStatusChange = async (newStatus: string) => {
-    const selectedTrackingCodes = Object.keys(selectedOrders).filter(
-      (trackingCode) => selectedOrders[trackingCode]
+  const handleDragEnd = (event: any) => {
+    const { active, over } = event;
+
+    if (!over || active.id === over.id) return;
+
+    const oldIndex = sendingTasks.findIndex(
+      (task: any) => task.tracking_code === active.id
+    );
+    const newIndex = sendingTasks.findIndex(
+      (task: any) => task.tracking_code === over.id
     );
 
-    if (selectedTrackingCodes.length === 0) {
-      alert(t("No orders selected!"));
-      return;
-    }
+    console.log(`From index: ${oldIndex - 1}, tracking code: ${active.id}`);
 
-    try {
-      // Update statuses locally
-      const updatedTasks = sendingTasks.map((task: any) =>
-        selectedTrackingCodes.includes(task.tracking_code)
-          ? { ...task, Status: newStatus }
-          : task
-      );
-      setSendingTasks(updatedTasks);
-
-      const params = {
-        device_id: userInfo.device_id,
-        status: newStatus,
-        orders: selectedTrackingCodes,
-      };
-
-      const response = await changeOrderStatus(params);
-      console.log("Order statuses updated successfully:", response);
-
-      setSelectedOrders({});
-      setCheckAll(false);
-
-      alert(t("Selected orders updated successfully!"));
-    } catch (error: any) {
-      console.error("Failed to update order statuses:", error);
-      alert(t("Failed to update orders. Please try again."));
-    }
-
-    await fetchUpdatedOrderList();
+    const newOrder = arrayMove(sendingTasks, oldIndex, newIndex);
+    setSendingTasks(newOrder);
+    console.log(`To index: ${newIndex - 1}, tracking code: ${over.id}`);
   };
-
   if (!sendingTasks || sendingTasks.length === 0) {
     return <p className="text-center text-gray-500">{t("you have no task")}</p>;
   }
@@ -118,7 +163,7 @@ const Order = ({ status }: { status: string | null }) => {
   return (
     <div className="relative px-4">
       <div className="sticky top-0 z-10 flex items-center bg-white">
-      <div className="flex items-center border-2 border-gray-300 w-full rounded-md px-4 py-2">
+        <div className="flex items-center border-2 border-gray-300 w-full rounded-md px-4 py-2">
           <FontAwesomeIcon icon={faBarcode} className="text-gray-500 mr-2" />
           <input
             type="text"
@@ -130,64 +175,24 @@ const Order = ({ status }: { status: string | null }) => {
         </div>
       </div>
 
-      {status === "Waiting" && filteredTasks.length > 0 && (
-        <div className="flex items-center gap-2 py-2 px-3 border-b-2 border-gray-500">
-          <div className="flex items-center gap-2">
-            <input
-              type="checkbox"
-              checked={checkAll}
-              onChange={(e) => handleCheckAllChange(e.target.checked)}
-              className="h-5 w-5 text-yellow-600 rounded border-gray-300 focus:ring-yellow-500"
-            />
-            <span>{t("select all")}</span>
-          </div>
-          <button
-            onClick={() => handleAllStatusChange("Accepted")}
-            className="ml-auto px-4 py-2 bg-yellow-400 text-black text-sm font-semibold rounded-md"
-          >
-            {t("accept all")}
-          </button>
-        </div>
-      )}
-
-      {filteredTasks.map((item: any, index: number) => (
-        <div
-          key={index}
-          className="first:border-t-2 border-b-2 py-2 px-3 border-gray-500 flex gap-4"
+      <DndContext collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+        <SortableContext
+          items={filteredTasks.map((task: any) => task.tracking_code)}
+          strategy={verticalListSortingStrategy}
         >
-          {status === "Waiting" && (
-            <div className="flex items-center gap-2 mt-2">
-              <input
-                type="checkbox"
-                checked={!!selectedOrders[item.tracking_code]}
-                onChange={(e) =>
-                  handleCheckboxChange(item.tracking_code, e.target.checked)
-                }
-                className="h-5 w-5 text-yellow-600 rounded border-gray-300 focus:ring-yellow-500"
-              />
-            </div>
-          )}
-
-          <div
-            className="w-full flex flex-col gap-1 cursor-pointer"
-            onClick={() => navigate(`/order/${item.tracking_code}`)}
-          >
-            <div className="flex justify-between">
-              <h2 className="text-sm">{item.client_name}</h2>
-              <p className="text-sm">{item.sum} ₾</p>
-            </div>
-            <div className="flex items-center gap-1">
-              <FontAwesomeIcon icon={faBarcode} />
-              <p className="text-sm">{item.tracking_code}</p>
-            </div>
-            <div className="flex items-center gap-1">
-              <FontAwesomeIcon icon={faPhone} />
-              <p className="text-sm">{item.client_phone}</p>
-            </div>
-            <h2 className="text-sm">{item.client_address}</h2>
-          </div>
-        </div>
-      ))}
+          {filteredTasks.map((item: any) => (
+            <SortableItem
+              key={item.tracking_code}
+              id={item.tracking_code}
+              task={item}
+              status={status}
+              navigate={navigate}
+              handleCheckboxChange={handleCheckboxChange}
+              selectedOrders={selectedOrders}
+            />
+          ))}
+        </SortableContext>
+      </DndContext>
     </div>
   );
 };
