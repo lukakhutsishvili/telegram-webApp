a563ccd286e4d4343b11c0ac84dd7592087b6b5b
Luka Khutsishvili <lukakhutsishvili777@gmail.com>
2025-01-13 01:41:57 +0400

    fix

--- src/components/Scanner.tsx
+++ src/components/Scanner.tsx
@@ -114,7 +114,7 @@ const BarcodeScanner = () => {
   };
 
   return (
-    <div className="relative flex flex-col items-center justify-start h-screen bg-gray-100 pt-12">
+    <div className="relative flex flex-col items-center justify-start h-screen bg-gray-100 pt-24">
       {!isModalOpen && (
         <div className="grid justify-center w-full max-w-md mx-auto">
           <video
