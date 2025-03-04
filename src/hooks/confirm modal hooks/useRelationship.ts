3889f5540184c9bab2d5638f8f69da6b709811cf
Anri Tsetskhlashvili <67234362+anritsetskhla10@users.noreply.github.com>
2025-03-04 12:02:40 +0400

    relationship update

--- src/hooks/confirm modal hooks/useRelationship.ts
+++ src/hooks/confirm modal hooks/useRelationship.ts
@@ -5,7 +5,7 @@ import { axiosInstance } from "../../api/apiClient";
 
 const useRelationships = (autoFetch: boolean = true) => {
   const { userInfo } = useContext(Context);
-  const [data, setData] = useState(null);
+  const [relationshipData, setRelationshipData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
@@ -18,13 +18,9 @@ const useRelationships = (autoFetch: boolean = true) => {
     setLoading(true);
     setError(null);
 
-    const params = {
-      device_id: userInfo.device_id,
-    };
-
     try {
-      const response = await axiosInstance.get(GET_RELATIONSHIPS, { params });
-      setData(response.data);
+      const response = await axiosInstance.get(GET_RELATIONSHIPS);
+      setRelationshipData(response.data);
       return response.data;
     } catch (err) {
       setError("Failed to fetch relationships");
@@ -40,7 +36,7 @@ const useRelationships = (autoFetch: boolean = true) => {
     }
   }, [autoFetch]);
 
-  return { data, loading, error, getRelationships };
+  return { relationshipData, loading, error, getRelationships };
 };
 
 export default useRelationships;
